07621d73c28f2d9d8a2048cd26765e143fac6cdc
kiboiWanjohi <sammy313.sw@gmail.com>
2023-11-20 22:53:24 -0800

    :white_check_mark: 0x05 Node JS Basic

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,19 +2,21 @@ def makeChange(coins, total):
     """Determines the fewest number of coins needed to meet a given
     amount total when given a pile of coins of different values.
     """
+
     if total <= 0:
         return 0
+
     rem = total
     coins_count = 0
-    coin_idx = 0
+    coin_no = 0
     sorted_coins = sorted(coins, reverse=True)
     n = len(coins)
     while rem > 0:
-        if coin_idx >= n:
+        if coin_no >= n:
             return -1
-        if rem - sorted_coins[coin_idx] >= 0:
-            rem -= sorted_coins[coin_idx]
+        if rem - sorted_coins[coin_no] >= 0:
+            rem -= sorted_coins[coin_no]
             coins_count += 1
         else:
-            coin_idx += 1
+            coin_no += 1
     return coins_count
