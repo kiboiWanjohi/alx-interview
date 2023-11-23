bfe5265c53fa5c59bac60b300d0ed8a55015c506
kiboiWanjohi <sammy313.sw@gmail.com>
2023-11-23 02:09:36 -0800

    :hammer: 0x08-Making_Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def makeChange(coins, total):
     """Determines the fewest number of coins needed to meet a given
     amount total when given a pile of coins of different values.
